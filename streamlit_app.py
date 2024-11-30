9a81e48fd83f87d1b7ac65e2645e14304b61701f
Billionaire-cmd <rabbigyimah7@gmail.com>
2024-11-30 14:44:17 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,7 +48,7 @@ def calculate_predictions():
     st.sidebar.header("Team Strengths")
     home_attack = st.sidebar.number_input("Home Attack Strength", value=2.39, format="%.2f")
     home_defense = st.sidebar.number_input("Home Defense Strength", value=0.56, format="%.2f")
-    away_attack = st.sidebar.number_input("Away Attack Strength", value=1.20, format="%.2f")
+    away_attack = st.sidebar.number_input("Away Attack Strength", value=1.20, format="%.f")
     away_defense = st.sidebar.number_input("Away Defense Strength", value=1.33, format="%.2f")
 
     # Submit Button
