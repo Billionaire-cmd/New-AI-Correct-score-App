fee543c7ab0dd87ae68b1f2148aaef6473146116
Billionaire-cmd <rabbigyimah7@gmail.com>
2024-12-23 19:39:36 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,8 +59,8 @@ prob_matrix = np.outer(team_a_probs, team_b_probs)
 
 # Calculate outcome probabilities
 home_win_prob = np.sum([prob_matrix[i, j] for i in range(max_goals - 2) for j in range(max_goals - 1) if i > j])
-draw_prob = np.sum([prob_matrix[i, j] for i in range(max_goals - 2) for j in range(max_goals - 1) if i < j])
-away_win_prob = np.sum([prob_matrix[i, j] for i in range(max_goals - 2) for j in range(max_goals - 1) if i == j])
+draw_prob = np.sum([prob_matrix[i, j] for i in range(max_goals - 2) for j in range(max_goals - 1) if i == j])
+away_win_prob = np.sum([prob_matrix[i, j] for i in range(max_goals - 2) for j in range(max_goals - 1) if i < j])
 
 # Weight probabilities by odds
 weighted_home_win_prob = home_win_prob / home_odds
